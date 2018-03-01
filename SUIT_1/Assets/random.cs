f6d944e36a5415f257a465bd0349214fdaec3c34
ZacharyPeters <36746461+ZacharyPeters@users.noreply.github.com>
2018-03-01 16:19:44 -0500

    changed random
    i changed stuffs

--- SUIT_1/Assets/random.cs
+++ SUIT_1/Assets/random.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class random : MonoBehaviour {
+public class random : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float YAxisRotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, YAxisRotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
 }
