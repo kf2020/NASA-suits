9a4ebe6e7797a141c83bc84f9beb5ab72d43a716
Katie F <katieefox@comcast.net>
2018-03-01 16:11:53 -0500

    changing random 2

--- SUIT_1/Assets/random.cs
+++ SUIT_1/Assets/random.cs
@@ -6,7 +6,8 @@ public class random : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		int i = 0;
+		string hello = "hello world!";
 	}
 	
 	// Update is called once per frame
