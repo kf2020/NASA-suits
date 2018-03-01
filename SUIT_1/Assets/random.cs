53d2c778e2ee4efd01fab127c37752a3aae84016
bsack <bsack@terpmail.umd.edu>
2018-03-01 16:14:53 -0500

    Brady Test
    Changed random.cs

--- SUIT_1/Assets/random.cs
+++ SUIT_1/Assets/random.cs
@@ -14,3 +14,4 @@ public class random : MonoBehaviour {
 		
 	}
 }
+// Random Random Random Random Random  Random Random Random Random Random  Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random
