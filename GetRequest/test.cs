6f79b9fec3994e8d3b14a2ed03bf26c53a232a6e
bsack <bsack@terpmail.umd.edu>
2018-03-07 20:17:05 -0500

    GetRequestTest
    testing http get request

---
+++ GetRequest/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+public class Class1
+{
+    string sURL = "http://NASASuits";
+
+    WebRequest wrGetURL = wrGetURL.Create(sURL);
+    //wrGetURL = WebRequest.Create(sURL);
+
+
+
+
+}
